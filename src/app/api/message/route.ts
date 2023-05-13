216f754ebc68710c2fad6d7f613030730a5a8159
Cdemz <gbengaadepoju75@gmail.com>
2023-05-13 15:06:42 +0100

    first commit

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,20 @@
+import { chatbotPrompt } from "@/app/helpers/constants/chatbox-prompt"
+import { ChatGPTMessage } from "@/lib/openai-stream"
+import { MessageArraySchema } from "@/lib/validators/message"
+
+export async function POST(req: Request) {
+  const{message} = await req.json()
+
+  const parsedMessages = MessageArraySchema.parse(messages)
+
+  const outboundMessages: ChatGPTMessage[] = parsedMessages.map((message) => {
+    {
+        role: message.isUserMessage ? "user" : "system",
+        content: message.text,
+    }})
+
+    outboundMessages.unshift({
+        role: "system",
+        content: chatbotPrompt,
+    })
+}
